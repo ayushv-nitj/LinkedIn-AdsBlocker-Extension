2f5ad23ee65c8155261d4e8246d8bed7078606b0
ayushv-nitj <ayush7610jsr@gmail.com>
2025-06-29 12:17:18 +0530

    update linkedin.js

--- linkedin.js
+++ linkedin.js
@@ -1,35 +1,30 @@
+let adsBlocked = 0;
+
 function removeAds() {
+  const spans = document.querySelectorAll("span");
 
-    // Get all 'span' elements on the page
-    let spans = document.getElementsByTagName("span");
+  spans.forEach(span => {
+    //  check if the span text is exactly "Promoted"
+    if (span.innerText.trim() === "Promoted") {
+      // Traverse upward until we find a post container
+      let adElement = span.closest("div[data-urn]");
 
-    for (let i = 0; i < spans.length; ++i) {
-        // Check if they contain the text 'Promoted'
-        if (spans[i].innerHTML === "Promoted") {
-            // Get the div that wraps around the entire ad
-            let card = spans[i].closest(".feed-shared-update-v2");
+      if (adElement && adElement.style.display !== "none") {
+        // Doublecheckin there's nothing meaningful the user would miss
+        adElement.style.display = "none";
 
-            // If the class changed and we can't find it with closest(), get the 6th parent
-            if (card === null) {
-                // Could also be card.parentNode.parentNode.parentNode.parentNode.parentNode.parentNode :D
-                let j = 0;
-                card = spans[i];
-                while (j < 6) {
-                    card = card.parentNode;
-                    ++j;
-                }
-            }
+          console.log("Blocked an ad:", adElement);
+        adsBlocked++;
 
-            // Make the ad disappear!
-            card.setAttribute("style", "display: none !important;");
-        }
+        // Store the updated ad count
+        chrome.storage.local.set({ adsBlocked });
+      }
     }
+  });
 }
 
-
+// Initial run
 removeAds();
 
-// Ensures ads will be removed as the user scrolls
-setInterval(function () {
-    removeAds();
-}, 100)
+// Continue to clean as user scrolls
+setInterval(removeAds, 1000);
