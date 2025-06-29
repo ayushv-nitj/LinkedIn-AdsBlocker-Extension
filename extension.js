411f50e85676adf24cd622d6be09e2085e33b1b5
ayushv-nitj <ayush7610jsr@gmail.com>
2025-06-29 12:20:01 +0530

    update extension.js

--- extension.js
+++ extension.js
@@ -1,12 +1,11 @@
-// This background script runs as a service worker in Manifest V3
 
-// Triggered whenever the user navigates to a new page or refreshes
+// This eventlisteer is triggered whenever the user navigates to a new page or refreshes
 chrome.webNavigation.onCommitted.addListener((details) => {
-  // Only run on the main frame (not iframes)
+  // Only run on the main frame & not iframes
   if (details.frameId === 0) {
-    // Check if the user is on a LinkedIn page
+    // Checking if the user is on a LinkedIn page
     if (details.url.includes("linkedin.com")) {
-      // Inject the content script (linkedin.js) into the current tab
+     
       chrome.scripting.executeScript({
         target: { tabId: details.tabId },
         files: ['linkedin.js']
