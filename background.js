77122d5c6aff5477d0185a0f6c1eb790b11c71ac
ayushv-nitj <ayush7610jsr@gmail.com>
2025-06-29 10:56:32 +0530

    add background.js file

---
+++ background.js
@@ -0,0 +1,26 @@
+chrome.webNavigation.onCommiteed.addListener(function(tab){
+  
+    if(tab.frameId ==0){
+        chrome.tabs.query({ active : true, lastFocusedWindow : true}, tabs => {
+       
+
+            let url = tabs[0].url;
+            let parsdUrl = url.replace("https://","").replace("http://", "").replace("www.", "")
+
+            let domain = parsedUrl.slice(0, parsedUrl.indexOf('/') == -1 ? parsedUrl.length : parsedUrl.indexOf('/')).slice(0, parsedUrl.indexOf('?') == -1 ? parsedUrl.length : parsedUrl.indexOf('?'));
+
+            try {
+                if (domain.length < 1 || domain === null || domain === undefined) {
+                    return;
+                } else if ( domain == "linkedin.com"){
+                    runLinkedinScript();
+                    return;
+                }
+            } catch (err){
+                throw err;
+            }
+
+        });
+
+    }
+});
